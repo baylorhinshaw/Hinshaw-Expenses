33173a958777b96a8d7b6516a6f5672aed3434df
Baylor <baylorhinshaw21@gmail.com>
2022-03-11 20:13:01 -0500

    models need to be checked

--- server/models/Expenses.js
+++ server/models/Expenses.js
@@ -1,23 +1,29 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const { Schema } = mongoose;
 
 const expensesSchema = new Schema({
-    monthlyIncome: {
-      type: Number
-    },
-    fixedExpenses: {
-      type: Number
-    },
-    varibleExpenses: {
-      type: Number
-    },
-    savings: {
-      type: Number,
-    },
-    leftoverIncome: {
-      type: Number,
-    }
+  monthlyIncome: {
+    type: Number
+  },
+  fixedExpenses: {
+    type: Number
+  },
+  varibleExpenses: {
+    type: Number
+  },
+  savings: {
+    type: Number,
+  },
+  leftoverIncome: {
+    type: Number,
+  },
+  // put other schemas
+},
+{
+  toJSON: {
+    virtuals: true
+},
 });
 
 module.exports = expensesSchema;
--- server/models/User.js
+++ server/models/User.js
@@ -3,35 +3,33 @@ const bcrypt = require('bcrypt');
 
 const expensesSchema = require('./Expenses')
 
-const userSchema = new Schema(
-    {
-        firstName: {
-            type: String,
-            required: true
-        },
-        lastName: {
-            type: String, 
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [/.+@.+\..+/, 'Must use a valid email address']
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        expenses: [expensesSchema],
-    },
-    // need this to use Virtual
-    {
-        toJSON: {
-            virtuals: true
-        },
-    }
-);
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must use a valid email address']
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  expenses: [expensesSchema]
+},
+  // need this to use Virtual
+{
+  toJSON: {
+    virtuals: true
+},
+});
 
 // hash user password
 userSchema.pre('save', async function (next) {
